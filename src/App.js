59b73086f8089a7978aa9ee47513a5e0f6cc79b6
johnstuart670 <jbenfellstuart@gmail.com>
2018-08-27 17:49:32 -0600

    first push

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h1 className="App-title">ERC Phone Server</h1>
+        <p className="App-intro">
+          Testing the server capabilities of Node and Heroku to run a "Your Client is here" notification        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
