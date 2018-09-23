ed699f641014f1a4385edec8033c85c6cc4717ba
johnstuart670 <jbenfellstuart@gmail.com>
2018-09-22 21:01:09 -0600

    letting the 1 npm go through for mdbreact

--- client/src/Components/Pages/Checkin.js
+++ client/src/Components/Pages/Checkin.js
@@ -29,14 +29,14 @@ export default class Checkin extends Component {
         return (
             <React.Fragment>
                 <Container className="mx-auto px-0 py-5 ">
-                    <h1 className="mx-auto my-0 text-center override">ERC</h1>
-                    <h4 className="mx-auto my-1 text-center override"> Who are you here to see today?
+                    <h1 className="mx-auto my-0 text-center brown-text">ERC</h1>
+                    <h4 className="mx-auto my-1 text-center brown-text"> Who are you here to see today?
            </h4>
-                    <Button block id="Jacob"   className="mx-auto my-3 col-7 black-text amber lighten-3" onClick={() => { this.pingT("Jacob") }} ><h4 className ="my-auto black-text ">Jacob Jacquez, LCSW</h4></Button>
-                    <Button block id="Amanda"   className="mx-auto my-3 col-7  black-text amber lighten-3" onClick={() => { this.pingT("Amanda") }}><h4 className ="my-auto black-text">Amanda Gonzalez, LCSW</h4></Button>
-                    <Button block id="Brenda"   className="mx-auto my-3 col-7  black-text amber lighten-3" onClick={() => { this.pingT("Brenda") }}><h4 className ="my-auto black-text">Brenda Dowse, LCSW</h4></Button>
-                    <Button block id="Ana"   className="mx-auto my-3 col-7  black-text amber lighten-3" onClick={() => { this.pingT("Ana") }}><h4 className ="my-auto black-text">Ana Borges, LMFT</h4></Button>
-                    <Button block id="Myles"   className="mx-auto my-3 col-7  black-text amber lighten-3" onClick={() => { this.pingT("Myles") }}><h4 className ="my-auto black-text">Myles Davis, LCSW</h4></Button>
+                    <Button block id="Jacob" color="brown" className="mx-auto my-3 col-7 black-text" onClick={() => { this.pingT("Jacob") }} ><h4 className="my-auto black-text">Jacob Jacquez, LCSW</h4></Button>
+                    <Button block id="Amanda" color="brown" className="mx-auto my-3 col-7  black-text " onClick={() => { this.pingT("Amanda") }}><h4 className="my-auto black-text">Amanda Gonzalez, LCSW</h4></Button>
+                    <Button block id="Brenda" color="brown" className="mx-auto my-3 col-7  black-text " onClick={() => { this.pingT("Brenda") }}><h4 className="my-auto black-text">Brenda Dowse, LCSW</h4></Button>
+                    <Button block id="Ana" color="brown" className="mx-auto my-3 col-7  black-text " onClick={() => { this.pingT("Ana") }}><h4 className="my-auto black-text">Ana Borges, LMFT</h4></Button>
+                    <Button block id="Myles" color="brown" className="mx-auto my-3 col-7  black-text " onClick={() => { this.pingT("Myles") }}><h4 className="my-auto black-text">Myles Davis, LCSW</h4></Button>
                 </Container>
             </React.Fragment>
 
--- client/src/Components/Pages/LandingPage.js
+++ client/src/Components/Pages/LandingPage.js
@@ -18,12 +18,12 @@ export default class LandingPage extends Component {
         return (
             <React.Fragment>
                 <Container className="mx-auto">
-                    <p  className="text-center pt-4 mx-auto SuperText amber-text ">Thank you.</p>
-                    <p  className="text-center pt-0 mx-auto amber lighten-3 SuperText">Please have a seat.</p>
-                    <p  className="text-center pt-1 mx-auto lineAdjust SuperText amber-text">Your counselor has been notified of your arrival.</p>
+                    <p  className="text-center pt-4 mx-auto SuperText brown-text ">Thank you.</p>
+                    <p  className="text-center pt-0 mx-auto brown SuperText">Please have a seat.</p>
+                    <p  className="text-center pt-1 mx-auto lineAdjust SuperText brown-text">Your counselor has been notified of your arrival.</p>
                 </Container>
                 <Container className = "btnPosition pt-5">
-                    <Button block className="amber lighten-3 bottomBtn" onClick={() => { this.updateParentState() }}>
+                    <Button block color = "brown" className=" bottomBtn" onClick={() => { this.updateParentState() }}>
                         <h3 className="my-auto black-text">Go back to the Checkin Page</h3>
                     </Button>
                 </Container>
